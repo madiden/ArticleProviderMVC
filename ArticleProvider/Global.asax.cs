d6dc41b22624e6caacada0474c2a17a591e4a724
mahmuduman <mahmuduman@DESKTOP-QHH00FD>
2017-05-13 00:19:56 +0300

    fix

--- ArticleProvider/Global.asax.cs
+++ ArticleProvider/Global.asax.cs
@@ -6,11 +6,6 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
-<<<<<<< HEAD
-
-//lll
-=======
->>>>>>> 3cbdc1d... Add project files.
 namespace ArticleProvider
 {
     public class MvcApplication : System.Web.HttpApplication
